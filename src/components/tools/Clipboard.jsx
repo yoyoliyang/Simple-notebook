4203f19455bf64cb2d350a0fcf39595395583dbe
yoyoliyang@gmail.com <yoyoliyang@gmail.com>
2020-08-22 07:57:42 +0800

    修正插入markdown图像链接后光标修正

--- src/components/tools/Clipboard.jsx
+++ src/components/tools/Clipboard.jsx
@@ -1,8 +1,11 @@
 import React from "react"
 import { imgApi } from "./Env"
+import { useEffect, useRef, useState } from "react"
 const Clipboard = (props) => {
 
 
+    const [cursor, setCursor] = useState(0)
+    const textareaRef = useRef()
     // value为e.target.value数据，cursorPostion为当前光标位置，在光标位置插入剪切板数据
     const clip = async (value, cursorPosition) => {
         let clipData = await navigator.clipboard.read()
@@ -15,11 +18,17 @@ const Clipboard = (props) => {
             const timestamp = new Date().getTime()
             const imgName = `${props.blogData._id}--${timestamp}`
             let valueLength = value.length
-            let data = value.slice(0, cursorPosition) + `![${imgName}](${imgApi}/${imgName})` + value.slice(cursorPosition, valueLength)
+            let markdownText = `![${imgName}](${imgApi}/${imgName})` + '\n'
+            let data = value.slice(0, cursorPosition) + markdownText + value.slice(cursorPosition, valueLength)
             props.handleInsertImage(data, imgName, base64data)
+            setCursor(cursorPosition + markdownText.length)
         }
     }
 
+    useEffect(() => {
+        //  处理光标位置，markdown字符串后
+        textareaRef.current.selectionStart = textareaRef.current.selectionEnd = cursor
+    }, [cursor])
 
     const handleKeyDown = (e) => {
         if (e.ctrlKey) {
@@ -27,15 +36,15 @@ const Clipboard = (props) => {
                 let value = e.target.value
                 let cursorPosition = e.target.selectionStart
                 clip(value, cursorPosition)
-                // 此处需要处理光标位置返回问题
             }
         }
     }
 
+
     return (
         <div className="form-group">
             <label htmlFor="data" />
-            <textarea  rows="10" className="form-control" onKeyDown={(e) => handleKeyDown(e)} name="data" value={props.blogData.data} onChange={(e) => props.handleEdit(e)} placeholder="内容(支持markdown)" required />
+            <textarea id='text' rows="10" ref={textareaRef} className="form-control" onKeyDown={(e) => handleKeyDown(e)} name="data" value={props.blogData.data} onChange={(e) => props.handleEdit(e)} placeholder="内容(支持markdown)" required />
         </div>
     )
 }
